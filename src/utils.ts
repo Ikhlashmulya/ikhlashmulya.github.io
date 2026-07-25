70f7956cff15f8eb519b43be7cb7daa31a65495e
Ikhlashmulya <nurahmanmulya@gmail.com>
2026-07-25 12:30:48 +0800

    delete everything

--- src/utils.ts
+++
@@ -1,8 +0,0 @@
-import { getCollection, type CollectionEntry } from "astro:content";
-
-export async function getSortedPosts() {
-  const posts = (await getCollection("blog"))
-  .sort((a, b) => b.data.pubDate.valueOf() - a.data.pubDate.valueOf()) as CollectionEntry<"blog">[];
-
-  return posts; 
-}
